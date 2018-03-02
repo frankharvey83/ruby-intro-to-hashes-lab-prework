b2e8093bf0dca3c863724e380ff875c7afc15fc2
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-02 01:52:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,14 +9,14 @@ end
 
 def monopoly
 	monopoly = {
-		railroads => {}
+		:railroads => {}
 	}
 
 end
 
 def monopoly_with_second_tier
 	monopoly = {
-		railroads => {:pieces}
+		:railroads => {pieces: }
 	}
 end
 
